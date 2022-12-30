ae7a479a5a506042199f43f0333620dbffeca018
Matarov <moe.matar1998@gmail.com>
2022-12-30 17:00:41 +0200

    menu bar

--- src/Components/CategorySubmenu/index.js
+++ src/Components/CategorySubmenu/index.js
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function CategorySubmenu() {
   return (
-      <div>Category submenu</div>
+    <div>
+      <ul>
+        <li><a href="#">All Categories</a></li>
+        <li><a href="#">Fashion</a></li>
+        <li><a href="#">Furniture</a></li>
+        <li><a href="#">Kids toys</a></li>
+        <li><a href="#">Furniture</a></li>
+        <li><a href="#">Kitchen</a></li>
+        <li><a href="#">Books</a></li>
+        <li><a href="#">Sports</a></li>
+        <li><a href="#">Sports</a></li>
+      </ul>
+    </div>
   )
 }
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import logo from '../../images/logo.png'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { IoMdPerson, IoMdBasket } from 'react-icons/io'
+
+import "./style.css"
 
 export default function Navbar() {
   return (
-      <div>Navbar</div>
+    <div className='navbar_container'>
+      <div className='navbar_logo'>
+        <img src={logo} alt="navbar-logo" />
+        <p> Store</p>
+      </div>
+
+      <div className='navbar_searchbar'>
+        <input className='ipt-placeholder' type="text" placeholder="What are you looking for ?" />
+        <AiOutlineSearch  className='navbar-search-icon'/>
+      </div>
+      <div className='navbar_static_icon'>
+        <IoMdPerson />
+        <IoMdBasket/>
+      </div>
+    </div>
   )
 }
